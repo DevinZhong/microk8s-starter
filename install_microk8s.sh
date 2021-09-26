3d2d2e651a1f1de38e4624921f91bb04f164238f
Devin Zhong <devinzhong@outlook.com>
2021-09-26 11:08:06 +0800

    feat: update script

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo snap install microk8s --classic
 sudo snap alias microk8s.kubectl kubectl
-sudo usermod -a -G microk8s devin
-sudo chown -f -R devin ~/.kube
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
 newgrp microk8s
 
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1
--- login_with_ssh.sh
+++ login_with_ssh.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# 编辑 /etc/ssh/sshd_config，允许密码登录：
+# PasswordAuthentication yes
+# 重启 sshd 服务：
+# service sshd restart
+
 ssh-copy-id -i ~/.ssh/id_rsa.pub way2hacker.com
