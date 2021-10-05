011784fb47ac081c1a11c949ef5d0366e39bba79
Devin Zhong <devinzhong@outlook.com>
2021-10-05 21:02:59 +0800

    feat: setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_snap.sh | sh
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_docker.sh | sh
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_microk8s.sh | sh
