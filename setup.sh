33560b84ccf0de1bad51123ff4d1851baad5588f
Devin Zhong <devinzhong@outlook.com>
2021-10-05 23:46:30 +0800

    feat: update script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 # 直接运行本脚本
 # curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/setup.sh | /bin/bash
 
+sudo bash -c "echo \"$(curl -s https://websites.ipaddress.com/raw.githubusercontent.com | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -r | head -n 1)  raw.githubusercontent.com\" >> /etc/hosts"
+
 curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_snap.sh | /bin/bash
 source ~/.bashrc
 curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_docker.sh | /bin/bash
