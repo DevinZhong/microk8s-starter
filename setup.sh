f3e8123780ce40cfb7b11dc8d0dda3de4e9079a7
Devin Zhong <devinzhong@outlook.com>
2021-10-05 21:31:06 +0800

    feat: update script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 直接运行本脚本
-# curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/setup.sh | sh
+# curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/setup.sh | /bin/bash
 
-curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_snap.sh | sh
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_snap.sh | /bin/bash
 source ~/.bashrc
-curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_docker.sh | sh
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_docker.sh | /bin/bash
 source ~/.bashrc
-curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_microk8s.sh | sh
+curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/install_microk8s.sh | /bin/bash
