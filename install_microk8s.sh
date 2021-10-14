f33441887d9ec5d278cd2e6ca7aa7cce03e886fa
Devin Zhong <devinzhong@outlook.com>
2021-10-14 18:38:48 +0800

    feat: update script

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -15,12 +15,13 @@ sudo snap install microk8s --classic
 sudo snap alias microk8s.kubectl kubectl
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
-newgrp microk8s
 
 # 手动拉取 k8s.gcr.io/pause:3.1
+newgrp docker
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1 k8s.gcr.io/pause:3.1
 docker save k8s.gcr.io/pause:3.1 > pause.tar
+newgrp microk8s
 microk8s ctr image import pause.tar
 
 # microk8s.enable dns ingress dashboard
