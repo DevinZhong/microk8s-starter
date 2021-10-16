2fd54fd75210260cd3d730498f4994134421e581
Devin Zhong <devinzhong@outlook.com>
2021-10-16 12:25:52 +0800

    feat: update script

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -21,7 +21,7 @@ newgrp docker
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1 k8s.gcr.io/pause:3.1
 docker save k8s.gcr.io/pause:3.1 > pause.tar
-microk8s ctr image import pause.tar
+microk8s.ctr image import pause.tar
 
 microk8s.enable dns ingress dashboard
 
@@ -29,13 +29,13 @@ microk8s.enable dns ingress dashboard
 docker pull registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/ingress-nginx-controller:1.0.0-alpha.2
 docker tag registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/ingress-nginx-controller:1.0.0-alpha.2 k8s.gcr.io/ingress-nginx/controller:v1.0.0-alpha.2
 docker save k8s.gcr.io/ingress-nginx/controller:v1.0.0-alpha.2 > ingress-nginx-controller.tar
-microk8s ctr image import ingress-nginx-controller.tar
+microk8s.ctr image import ingress-nginx-controller.tar
 
 # 手动拉取 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
 docker pull registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/metrics-server:0.5.0
 docker tag registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/metrics-server:0.5.0 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
 docker save k8s.gcr.io/metrics-server/metrics-server:v0.5.0 > metrics-server.tar
-microk8s ctr image import metrics-server.tar
+microk8s.ctr image import metrics-server.tar
 
 # kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443 --address 0.0.0.0
 
