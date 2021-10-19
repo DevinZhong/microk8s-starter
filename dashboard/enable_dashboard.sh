3052ea1e36a81ed886696e65d2a870a8f3ca6a1c
Devin Zhong <devinzhong@outlook.com>
2021-10-19 21:57:40 +0800

    feat: enable_dashboard.sh

--- dashboard/enable_dashboard.sh
+++ dashboard/enable_dashboard.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # 手动拉取 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
 docker pull registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/metrics-server:0.5.0
 docker tag registry.cn-guangzhou.aliyuncs.com/devin-k8s-gcr-io/metrics-server:0.5.0 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
@@ -10,3 +9,5 @@ newgrp microk8s << EOF
 microk8s.ctr image import metrics-server.tar
 microk8s.enable dashboard
 EOF
+
+kubectl apply -f https://winter-glitter.devinzhong.workers.dev/DevinZhong/microk8s-starter/main/dashboard/dashboard-ingress.yaml
