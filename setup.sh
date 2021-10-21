3fb34d6634c50e6c8678ac887f5a137f1ae346ba
Devin Zhong <devinzhong@outlook.com>
2021-10-21 21:34:25 +0800

    feat: frps yaml

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 # 直接运行本脚本
 # source <(curl -SL https://raw.githubusercontent.com/DevinZhong/microk8s-starter/main/setup.sh)
+# 添加 host 后，可执行以下命令
+# source <(curl -SL https://winter-glitter.devinzhong.workers.dev/DevinZhong/microk8s-starter/main/setup.sh)
+
 
 # sudo bash -c "echo \"$(curl -s https://websites.ipaddress.com/raw.githubusercontent.com | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -r | head -n 1)  raw.githubusercontent.com\" >> /etc/hosts"
 sudo tee -a /etc/hosts <<-'EOF'
