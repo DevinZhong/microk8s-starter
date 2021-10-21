d7397a29b5a14bdb7ef3bba1f6cb770eaf77f109
Devin Zhong <devinzhong@outlook.com>
2021-10-21 22:39:01 +0800

    feat: update script

--- secret/install.sh
+++ secret/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 运行当前脚本
+# source <(curl -SL https://winter-glitter.devinzhong.workers.dev/DevinZhong/microk8s-starter/main/secret/install.sh)
+
 kubectl apply -f https://winter-glitter.devinzhong.workers.dev/DevinZhong/microk8s-starter/main/secret/nginx-tls-secret.yaml
 kubectl apply -f https://winter-glitter.devinzhong.workers.dev/DevinZhong/microk8s-starter/main/secret/dashboard-tls-secret.yaml
 
